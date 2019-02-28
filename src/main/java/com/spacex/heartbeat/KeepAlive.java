4a1a145eb8b61f2890592ed917fbe3b8c5b94733
lucas ley <ayaaa168@163.com>
2019-02-28 21:56:34 +0800

    feat(Server):add server for heart beat

---
+++ src/main/java/com/spacex/heartbeat/KeepAlive.java
@@ -0,0 +1,14 @@
+package com.spacex.heartbeat;
+
+import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class KeepAlive implements Serializable {
+    @Override
+    public String toString() {
+        Date current = new Date();
+        String message = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(current) + "\t keep alive package!";
+        return message;
+    }
+}
