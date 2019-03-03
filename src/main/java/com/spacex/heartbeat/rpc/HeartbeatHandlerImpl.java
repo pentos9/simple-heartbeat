a873f5edfef27378080c8e0e2f4d07e9f22ead20
lucas ley <ayaaa168@163.com>
2019-03-03 09:01:02 +0800

    feat(HeartbeatHandler):add heart beat handler

--- src/main/java/com/spacex/heartbeat/rpc/HeartbeatHandlerImpl.java
+++ src/main/java/com/spacex/heartbeat/rpc/HeartbeatHandlerImpl.java
@@ -1,8 +1,25 @@
 package com.spacex.heartbeat.rpc;
 
+import java.util.Map;
+
 public class HeartbeatHandlerImpl implements HeartbeatHandler {
 
     public Cmder handle(HeartbeatEntity entity) {
-        return null;
+        HeartbeatListener listener = HeartbeatListener.getInstance();
+        if (!listener.checkNodeValid(entity.getNodeId())) {
+            listener.registerNode(entity.getNodeId(), entity);
+        }
+
+        Cmder cmder = new Cmder();
+        cmder.setNodeId(entity.getNodeId());
+
+        //
+        System.out.println("list all nodes:");
+        Map<String, Object> nodes = listener.getNodes();
+        for (Map.Entry<String, Object> entry : nodes.entrySet()) {
+            System.out.println(String.format("%s:%s", entry.getKey(), entry.getValue()));
+        }
+        System.out.println("process and handle a heart beat!");
+        return cmder;
     }
 }
